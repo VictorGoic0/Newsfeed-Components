397ab8ae9d35d9a6c0e2bef90b3bab0dc8752f69
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-01-30 18:40:57 -0500

    removed repetitive CSS. Added new button to HTML, styled it in LESS file and added JS functionality to allow swapping of 'Expand' and 'Hide' buttons upon being clicked

--- Article/Article.js
+++ Article/Article.js
@@ -4,17 +4,22 @@ class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
     this.domElement = domElement;
+
+    this.exHiButtons = domElement.querySelector('.exHiButtons');
     // create a reference to the ".expandButton" class.
     this.expandButton = domElement.getElementsByClassName('expandButton')[0];
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.textContent = 'Expand';
+    // No
+    this.hideButton = domElement.getElementsByClassName('hideButton')[0];
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    this.expandButton.addEventListener('click', () => this.expandArticle());
+    this.exHiButtons.addEventListener('click', () => this.expandArticle());
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     this.domElement.classList.toggle('article');
+    this.expandButton.classList.toggle('close');
+    this.hideButton.classList.toggle('close');
   }
 }
 
