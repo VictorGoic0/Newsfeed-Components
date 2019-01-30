211295816e870d27079ca5598882f11f449c2a33
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-01-30 15:54:51 -0500

    toggleMenu actually works now

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,13 +1,13 @@
 
-const toggleMenu = (menu) => {
+const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence.
-  this.menuref = menu;
+  console.log('working');
   menu.classList.toggle('menu--open');
 }
 
 // Start Here: Create a reference to the ".menu" class
-const menu = Array.from(document.getElementsByClassName('menu'));
+const menu = document.querySelector('.menu');
 // create a reference to the ".menu-button" class
-const menuButton = Array.from(document.getElementsByClassName('menu-button'));
+const menuButton = document.getElementsByClassName('menu-button')[0];
 // Using your menuButton reference, add a click handler that calls toggleMenu
-menuButton.forEach(element => element.addEventListener('click', toggleMenu));
+menuButton.addEventListener('click', () => toggleMenu());
