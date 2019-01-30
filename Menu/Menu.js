cb83b0fda85850c2d534ae9cbfc7a63de2c06e15
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-01-30 15:35:22 -0500

    imported menu into index.less. menu.js function, reference variables and eventListeners added

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,13 @@
 
-const toggleMenu = () => {
-  // Toggle the "menu--open" class on your menu refence. 
+const toggleMenu = (menu) => {
+  // Toggle the "menu--open" class on your menu refence.
+  this.menuref = menu;
+  menu.classList.toggle('menu--open');
 }
 
 // Start Here: Create a reference to the ".menu" class
-const menu;
+const menu = Array.from(document.getElementsByClassName('menu'));
 // create a reference to the ".menu-button" class
-const menuButton;
+const menuButton = Array.from(document.getElementsByClassName('menu-button'));
 // Using your menuButton reference, add a click handler that calls toggleMenu
+menuButton.forEach(element => element.addEventListener('click', toggleMenu));
