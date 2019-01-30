aff8632c72692d216305c965590c3eadb86974fd
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-01-30 17:10:22 -0500

    unnecessary arrow function removed from callback in Menu.js. Article class in Article.js altered so that it references input instead of document. New article added to HTML. Styling for button added to Article.less

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,7 +1,6 @@
 
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence.
-  console.log('working');
   menu.classList.toggle('menu--open');
 }
 
@@ -10,4 +9,4 @@ const menu = document.querySelector('.menu');
 // create a reference to the ".menu-button" class
 const menuButton = document.getElementsByClassName('menu-button')[0];
 // Using your menuButton reference, add a click handler that calls toggleMenu
-menuButton.addEventListener('click', () => toggleMenu());
+menuButton.addEventListener('click', toggleMenu);
